0a869dfdeb351ca153eed043681a539e88fa6907
Screamer <vanagoncharov@mail.ru>
2024-02-22 18:25:04 +0300

    MVP SignUp, Room Database, Registration

--- app/src/main/java/com/example/gallery/model/User.kt
+++ app/src/main/java/com/example/gallery/model/User.kt
@@ -1,20 +1,9 @@
 package com.example.gallery.model
 
-import android.provider.ContactsContract.CommonDataKinds.Email
-import android.provider.ContactsContract.CommonDataKinds.Phone
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-import java.util.Date
-
-/**
- * Data class that captures user information for logged in users retrieved from LoginRepository
- */
-@Entity(tableName = "users")
 data class User(
-    @PrimaryKey val userId: String,
     val userName: String,
-    val birthday: Long?,
+    val birthday: String?,
     val phoneNumber: String,
     val email: String,
-    val password: String,
+    val password: String
 )
