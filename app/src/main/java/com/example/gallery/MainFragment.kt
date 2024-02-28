54e13b0fc76bf7d4aa19294697030cefd7b16060
Screamer <vanagoncharov@mail.ru>
2024-02-28 14:41:44 +0300

    recyclerView item's appearance, bottomNavigationView appearance, searchBar

--- app/src/main/java/com/example/gallery/MainFragment.kt
+++ app/src/main/java/com/example/gallery/MainFragment.kt
@@ -1,10 +1,12 @@
 package com.example.gallery
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -29,10 +31,9 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
-
         val bottomNavigationView = binding.bottomNavigationView
-        val navController = (childFragmentManager.findFragmentById(R.id.mainFragmentContainerView) as NavHostFragment).navController
+        val navController = (childFragmentManager.findFragmentById(R.id.mainFragmentContainerView
+        ) as NavHostFragment).navController
         bottomNavigationView.setupWithNavController(navController)
     }
 }
